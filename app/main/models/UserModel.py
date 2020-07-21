a333d33366256aff4fa46aca9dd1afcc098a0c00
Vikash Kumar <yadav.kk74780@gmail.com>
2020-07-21 12:09:19 +0530

    Add files via upload

---
+++ app/main/models/UserModel.py
@@ -0,0 +1,9 @@
+from app.main.settings import db
+
+class UserModel(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100),nullable = False)
+    email = db.Column(db.String(100),nullable = False,unique = True)
+    password = db.Column(db.String(50),nullable = False)
+    
