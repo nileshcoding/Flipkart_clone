b2004a2a3e562f36560c5666495066115fe9b434
Vikash Kumar <yadav.kk74780@gmail.com>
2020-07-21 16:03:03 +0530

    Update ProductModel.py

--- app/main/models/ProductModel.py
+++ app/main/models/ProductModel.py
@@ -1,1 +1,9 @@
-#product model
+from app.main.settings import db
+from . import CategoriesModel
+
+class ProductModel(db.Model):
+    __tablename__ = 'product'
+    id = db.Column(db.Integer, primary_key=True)
+    product_name=db.Column(db.String(100),nullable = False)
+    product_price=db.column(db.Float,nullable=False)
+    product_category_id=db.Column(db.Integer, db.ForeignKey('category.id'))
