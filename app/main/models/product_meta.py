b48975a05c1ea55960068aeef58fd3f0aaa5ad30
Vikash Kumar <yadav.kk74780@gmail.com>
2020-07-22 11:45:34 +0530

    Update product_meta.py

--- app/main/models/product_meta.py
+++ app/main/models/product_meta.py
@@ -1,1 +1,8 @@
-#productmeta
+from app.main.settings import db
+from . import ProductModel
+
+
+class Productmeta(db.model):
+    id=db.Column(db.Integer,primary_key=True)
+    image_path=db.Column(db.String(100),nullable = False)
+    product_id=db.Column(db.Integer, db.ForeignKey('product.id'))
